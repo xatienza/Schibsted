5fc01d73b6feb193e75c3818459497170037a831
Xavier Sanchez Atienza <xsancheza@cirsa.com>
2016-04-06 10:01:11 +0200

    Add Schibsted.WebSPA Project | Enable CORS in Web API | Added Auth controller and SignIn Method

---
+++ Schibsted.Web/App_Start/Startup.Auth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+namespace Schibsted.Web
+{
+    public partial class Startup
+    {
+        static Startup()
+        {
+          
+        }
+
+
+    }
+}
---
+++ Schibsted/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Schibsted.Models.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Schibsted.Controllers
+{
+    [RoutePrefix("api/Auth")]
+    [EnableCors("*", "*", "*")]
+    public class AuthController : ApiController
+    {
+        [HttpPost]
+        [Route("SignIn")]
+        public IHttpActionResult SignIn([FromBody]AuthenticateViewModel credentials)
+        {
+            if (String.IsNullOrEmpty(credentials.Username))
+                return InternalServerError(new ArgumentNullException("You have to set password"));
+
+            if (String.IsNullOrEmpty(credentials.Password))
+                return InternalServerError(new ArgumentNullException("You have to set role"));
+
+            var service = new Service.Security.SecurityService(WebApiApplication.mainRepository);
+
+            var result = service.User.Authenticate(credentials.Username, credentials.Password);
+
+            if (result != null)
+                return Ok(result);
+            else
+                return Unauthorized();
+        }
+    }
+}
